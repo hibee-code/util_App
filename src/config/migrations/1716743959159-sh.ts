91af147901e4a6b1abf6cc7cdde4b74c4a4e0e84
ibrahim <okeibrahim917@gmail.com>
2024-05-26 18:42:16 +0100

    create utility in db

--- src/config/migrations/1716743959159-sh.ts
+++ src/config/migrations/1716743959159-sh.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Sh1716740813548 implements MigrationInterface {
-    name = 'Sh1716740813548'
+export class Sh1716743959159 implements MigrationInterface {
+    name = 'Sh1716743959159'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "utility" ("id" BIGSERIAL NOT NULL, "name" character varying NOT NULL, "capacity" numeric NOT NULL, "userId" character varying NOT NULL, CONSTRAINT "PK_a231216e3ba8155f6b027f8d731" PRIMARY KEY ("id"))`);
--- src/utility/utility.controller.ts
+++ src/utility/utility.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UtilityService } from './utility.service';
+import { UtilityDto } from 'src/dto/utility.dto';
 
 @Controller('utility')
-export class UtilityController {}
+export class UtilityController {
+  constructor(private readonly utilsService: UtilityService) {}
+
+  @Post('create-util')
+  async create(@Body() utility: UtilityDto) {
+    return await this.utilsService.create(utility);
+  }
+}
